962b3c0b0f8845b162a720a475f648d674bb5cdd
pmb <p.boukhedimi@hotmail.com>
2023-03-13 11:56:14 +0100

    navbar-dropdown is now displayed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { Container } from "react-bootstrap";
 import { Navbar, Nav} from "react-bootstrap";
-import "bootstrap/dist/css/bootstrap.css"
+
 
 export const NavbarComponent = (props) => {
 
@@ -32,17 +32,21 @@ export const NavbarComponent = (props) => {
   }, []);
 
   return (
-    <Navbar display="flex" expand="sm">
+    <Navbar display="flex" expand="md">
       <Container>
         <Navbar.Brand>
         <Nav.Link as={NavLink} to="/">Panda</Nav.Link> 
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle aria-controls="basic-navbar-nav" class="dropdownIconBox">
+          <svg xmlns="http://www.w3.org/2000/svg" width="50" height="30" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>          
+        </Navbar.Toggle>
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
-            <Nav.Link as={NavLink} to="/Information">Information</Nav.Link>
-            <Nav.Link as={NavLink} to="/signOut">Sign Out</Nav.Link>
+            <Nav.Link as={NavLink} to="/" className="navBartextContent">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/Information" className="navBartextContent">Information</Nav.Link>
+            <Nav.Link as={NavLink} to="/signOut" className="navBartextContent">Sign Out</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
