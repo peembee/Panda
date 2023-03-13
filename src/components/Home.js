877e01addd9d5493e289e237a7fb3d8c3466a11c
Engwalls <erik.97@hotmail.com>
2023-03-13 16:27:06 +0100

    Designed the "home" page and added cards with different inputs.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,70 +1,211 @@
 import React from "react";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
+import hus from "../images/hus.png";
 
 export const HomeComponent = () => {
-    const boxStyle = {
-        padding: '10px',
-        marginBottom: '20px',
-        marginTop: '20px',
-        marginLeft: '20px'
-    };
 
-    return(
-        <div>           
-            <h1>Homepage, information om vad en användare kan göra? såsom </h1>
-            <div style={boxStyle}>    
-                <h3>box - skapa ett nytt projekt</h3>
-                <div>
-                    <Link to="/newProject">
-                        <Button>Go to page</Button>
-                    </Link>
+  return (
+    <div>
+      <main className="mainsection">
+      <div className="title">
+          <h3>En sida som räknar och håller koll på dina tider.</h3>
+          <h1>- Så att du slipper.</h1>
+        </div>
+        <div className="section">
+          <div className="card mb-3" style={{ maxWidth: "450px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={hus} className="img-fluid rounded-start" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Skapa nytt projekt</h5>
+                  <p className="card-text">Här skapar man ett nytt projekt.</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
                 </div>
+                <div>
+                  <Link to="/newProject">
+                    <Button>Skapa nytt projekt</Button>
+                  </Link>
+                </div>
+              </div>
             </div>
+          </div>
 
-            <div style={boxStyle}>
-                <h3>box - se aktiva projekt</h3>
-                <div>
-                    <Link to="/activeProjects">
-                        <Button>Go to page</Button>
-                    </Link>
+          <div className="card mb-3" style={{ maxWidth: "450px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={hus} className="img-fluid rounded-start" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Aktiva projekt</h5>
+                  <p className="card-text">Här ser man alla aktiva projekt.</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
                 </div>
+                <div>
+                  <Link to="/activeProjects">
+                    <Button>Aktiva projekt</Button>
+                  </Link>
+                </div>
+              </div>
             </div>
+          </div>
 
-            <div style={boxStyle}>
-                <h3>box - se tidigare projekt</h3>
-                <div>
-                    <Link to="/inactiveProjects">
-                        <Button>Go to page</Button>
-                    </Link>
+          <div className="card mb-3" style={{ maxWidth: "450px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={hus} className="img-fluid rounded-start" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Tidigare projekt</h5>
+                  <p className="card-text">Här ser man tidigare projekt.</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
                 </div>
+                <div>
+                  <Link to="/InactiveProjects">
+                    <Button>Tidigare projekt</Button>
+                  </Link>
+                </div>
+              </div>
             </div>
+          </div>
 
-            <div style={boxStyle}>
-                <h3>box - visa alla tidigare och aktuella projekt gjort av alla</h3>
-                <div>
-                     <Link to="/projectsList">
-                        <Button>Go to page</Button>
-                    </Link>
+          <div className="card mb-3" style={{ maxWidth: "450px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={hus} className="img-fluid rounded-start" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Lista med alla projekt</h5>
+                  <p className="card-text">Här ser man alla projekt</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
                 </div>
+                <div>
+                  <Link to="/projectsList">
+                    <Button>Projektlista</Button>
+                  </Link>
+                </div>
+              </div>
             </div>
+          </div>
 
-            <div style={boxStyle}>
-                <h3>box - Rapportera in tid på ett projekt en viss dag så jag kan visa vilken tid jag lagt ner</h3>
-                <div>
-                    <Link to="/reportTime">
-                        <Button>Go to page</Button>
-                    </Link>
+          <div className="card mb-3" style={{ maxWidth: "450px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={hus} className="img-fluid rounded-start" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Tidsrapportering</h5>
+                  <p className="card-text">Här rapporterar man tid för projektet.</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
                 </div>
-            </div>
-            <div style={boxStyle}>
-                <h3>Lägg till medarbetare</h3>
                 <div>
-                    <Link to="/addEmployee">
-                        <Button>Go to page</Button>
-                    </Link>
+                  <Link to="/timeLog">
+                    <Button>Tidsrapportering</Button>
+                  </Link>
                 </div>
+              </div>
             </div>
-        </div>        
-    )
-}
+          </div>
+
+          <div className="card mb-3" style={{ maxWidth: "450px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={hus} className="img-fluid rounded-start" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Lägg till personal</h5>
+                  <p className="card-text">Här lägger man till personal.</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+                <div>
+                  <Link to="/addEmployee">
+                    <Button>Lägg till</Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card mb-3" style={{ maxWidth: "450px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={hus} className="img-fluid rounded-start" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title"></h5>
+                  <p className="card-text">Här skapar man ett nytt projekt.</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+                <div>
+                  <Link to="/newProject">
+                    <Button>Skapa nytt projekt</Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="card mb-3" style={{ maxWidth: "450px" }}>
+            <div className="row g-0">
+              <div className="col-md-4">
+                <img src={hus} className="img-fluid rounded-start" alt="..." />
+              </div>
+              <div className="col-md-8">
+                <div className="card-body">
+                  <h5 className="card-title">Skapa nytt projekt</h5>
+                  <p className="card-text">Här skapar man ett nytt projekt.</p>
+                  <p className="card-text">
+                    <small className="text-muted">
+                      Last updated 3 mins ago
+                    </small>
+                  </p>
+                </div>
+                <div>
+                  <Link to="/newProject">
+                    <Button>Skapa nytt projekt</Button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
