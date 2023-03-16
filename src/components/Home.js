27fe15cb86f50d41f328b875e72a4af813aa9f67
pmb <p.boukhedimi@hotmail.com>
2023-03-16 12:32:37 +0100

    bugs fixing, new variable added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,4 @@
 import React from "react";
-import "../Css/newProject.css"
 import "../Css/style.css"
 import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
--- src/components/NewProject.js
+++ src/components/NewProject.js
@@ -8,109 +8,130 @@ export function NewProject() {
    const [show, setShow] = useState(false);
    const [newProject, setNewProject] = useState({      
       projectName: null,
-      description: null
+      description: null,
+      status: null
     });
-   
-    
-   const setName = e => {   // Local-functions for set the useState
-      setNewProject(values => ({
-         ...values,
+
+   //--------------------------------------------------------------------------------
+   // Local-functions for set the useState-variables
+
+   const setName = e => {   
+      setNewProject(getAllValues => ({
+         ...getAllValues,
          projectName: e.target.value,
       }))
     }
    const setDescription = e => {
-      setNewProject(values => ({
-         ...values,
+      setNewProject(getAllValues => ({
+         ...getAllValues,
          description: e.target.value,
       }))
     }
-
-
-    function handleModal(prop){ // handler closing of modal
-      if(prop){
-         setShow(false)
+    const setStatus = e => {
+      setNewProject(getAllValues => ({
+         ...getAllValues,
+         status: e.target.value,
+      }))
+    }
+  //--------------------------------------------------------------------------------
+   //--- preventing null-values to Swagger, if null - alert User
+    function checkInput(){ 
+      if(newProject.projectName === null || newProject.description === null || newProject.status === null){
+         return false;
       }
       else{
-         setShow(true)
+         if(newProject.projectName.trim() === "" || newProject.description.trim() === ""){
+            return false;
+         }
+         else{
+            return true;
+         }
       }
-    }
-
+   }
    
-    const PostToSwagger = (e) => {    
-      e.preventDefault();
-   
-      fetch('https://axb22z45ygh20230227215753.azurewebsites.net/create-project', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(newProject)
-      })
-        .then((response) => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          handleModal(); // popup modal active
+    const PostToSwagger = (e) => {      // Function for post to swagger
+      if(checkInput() === true){  // if all inputs has a value, post to Swagger
+         e.preventDefault();
+      
+         fetch('https://axb22z45ygh20230227215753.azurewebsites.net/create-project', {
+         method: 'POST',
+         headers: {
+            'Content-Type': 'application/json'
+         },
+         body: JSON.stringify(newProject)
+         })
+         .then((response) => {
+            if (!response.ok) {
+               throw new Error('Network response was not ok');
+            }
+            else{
+               setShow(true) // popup modal active
 
-
-          setNewProject({  // preventing duplicate from prev value
-            projectName: null,
-            description: null
-          });
-        })
-        .catch((error) => {
-          console.error('There was an error!', error);
-          alert('Error creating Project!');
-        });
+               setNewProject({  // preventing duplicate from prev value
+                  projectName: null,
+                  description: null,
+                  status: null
+               });
+             }
+         })
+         .catch((error) => {
+            console.error('There was an error!', error);
+            alert('Error creating Project!');
+         });
+      }
+      else{
+         alert("Vänligen fyll i alla rader")
+      }
     };
-   
+
 
   return (
       <form>
-         <div class="newProject">
-            <h1 class="title">Create a new Project by fill in the form</h1>      
-         
-            <label class="ProjectId">Project ID: #1011</label>       
+            <div class="newProject">
+               <h1 class="title">Create a new Project by fill in the form</h1>      
+            
+               <label class="ProjectId">Project ID: #1011</label>       
+
+               <div>
+                  <div class="row columnPlacer">
+                     <div class="col">
+                     <input name="projectName" type="text" class="form-control" placeholder="Project Name" onChange={setName} />
+                  </div>
+               </div>        
+            </div>
 
             <div>
                <div class="row columnPlacer">
-               <div class="col">
-                  <input name="projectName" type="text" class="form-control" placeholder="Project Name" onChange={setName} />
-               </div>
-            </div>        
+                  <div class="col">
+                     <input name="description" type="text" class="form-control" placeholder="Description" onChange={setDescription}/>
+                  </div>
+               </div>        
+            </div>
+            
+            <select class="form-select columnPlacer" aria-label="Default select example" onChange={setStatus}>
+               <option selected>Enter Status</option>
+               <option>Active</option>
+               <option>Inactive</option>
+            </select>
+
+            <Button type="submit" onClick={PostToSwagger}>Add Project</Button>
+            <Button id="resetBtn" type="reset">Reset</Button>
          </div>
 
-         <div>
-               <div class="row columnPlacer">
-               <div class="col">
-                  <input name="description" type="text" class="form-control" placeholder="Description" onChange={setDescription}/>
-               </div>
-            </div>        
-         </div>
-         
-         <select class="form-select columnPlacer" aria-label="Default select example">
-            <option selected>Active</option>
-            <option value="1">Inactive</option>
-         </select>
-
-         <Button type="submit" onClick={PostToSwagger}>Add Project</Button>
-         <Button type="reset">Reset</Button>
-         </div>
-
-         <Modal show={show}>
-              <Modal.Header closeButton onClick={handleModal}>
-                 <Modal.Title>Project Added</Modal.Title>
-              </Modal.Header>
-             <Modal.Body>
-                <div>
+         <Modal show={show} backdrop="static">
+               <Modal.Header closeButton onClick={() => setShow(false)}>
+                  <Modal.Title>Project Added</Modal.Title>
+               </Modal.Header>
+               <Modal.Body>
+                  <div>
                   A new Project has now been added, you can later find it in the List of Projects
-                </div>
-             </Modal.Body>
-             <Modal.Footer>
-            <Button variant="secondary" onClick={handleModal}>Close Modal</Button>
+                  </div>
+               </Modal.Body>
+               <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShow(false)}>Close Modal</Button>
             </Modal.Footer>
-      </Modal>
-      </form>
+         </Modal>
+   </form>
   )
 }
 
