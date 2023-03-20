46595c2acaff0e5ea15b2fb7420cf0a23d439cab
silvergripen <57497725+silvergripen@users.noreply.github.com>
2023-03-20 18:43:03 +0100

    Lade till kommmentars låda samt dropdown lista

--- src/components/AddComment.js
+++ src/components/AddComment.js
@@ -1,13 +1,93 @@
 import React from 'react'
-
+import '../Css/reportTime.css';
+import { Form } from 'react-bootstrap';
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Css/loading.css";
+import "../Css/displayEmployees.css";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 
 
 
 export function AddComment() {
+  const [error, setError] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [swaggerData, setSwaggerData] = useState([]);
 
-  return (
-    <div>
-        <h1>lägg en kommentar på ett projekt som är skapat av den inloggade användaren</h1>
+  useEffect(() => {
+    fetch(
+      "https://axb22z45ygh20230227215753.azurewebsites.net/get-all-projects"
+    )
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          setLoaded(true);
+          setSwaggerData(result);
+          console.log(result);
+        },
+        (error) => {
+          setLoaded(true);
+          setError(error);
+        }
+      );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!loaded) {
+    // loading data icon
+    return (
+      <>
+        <h2 class="loadingTitle">Loading</h2>
+        <div className="spinner-border text-danger loadingIcon" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </>
+    );
+  } else {
+     return (
+    <div className='comment'>
+          <Dropdown>
+            <Dropdown.Toggle variant="success" id="dropdown-basic">
+              projects
+            </Dropdown.Toggle>
+
+            <Dropdown.Menu>
+            {swaggerData.map((swaggerData) => (
+                <Dropdown.Item key={swaggerData.projectId}>
+                    {swaggerData.projectName}
+                  </Dropdown.Item>
+        ))}
+            </Dropdown.Menu>
+          </Dropdown>
+          <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label></Form.Label>
+                <Form.Control as="textarea" rows={5} placeholder="Comment" />
+              </Form.Group>
+          </Form>
     </div>
   )
+  }
 }
+
+
+// <div>
+//         <div className="title-container">
+//           <h1 className="displayTitle">Här är alla anställda i systemet</h1>
+//         </div>
+//         <div className="display-container">
+//           {swaggerData.map((swaggerData) => (
+//             <div className="display-module" key={swaggerData.employeeId}>
+//               <p>{"ID nummer: " + swaggerData.employeeId}</p>
+//               <p>{"Lösenord: " + swaggerData.password}</p>
+//               <p>{"Förnamn: " + swaggerData.firstMidName}</p>
+//               <p>{"Efternamn: " + swaggerData.lastName}</p>
+//               <p>{"Address: " + swaggerData.address}</p>
+//               <p>{"Stad: " + swaggerData.city}</p>
+//               <p>{"PostNummer: " + swaggerData.postalCode}</p>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
--- src/components/ReportTime.js
+++ src/components/ReportTime.js
@@ -1,19 +1,17 @@
-import React from 'react'
+import React from 'react';
 import { TimeLog } from "./TimeLog";
 import { AddComment } from "./AddComment";
+
+
 
 export function ReportTime() {
   return (
     <div>
-        <h1>Rapportera in tid på ett projekt en viss dag så jag kan visa vilken tid jag lagt ner</h1>        
+        <h1>comment Component</h1>        
         <div>
-          ------------------------------------
-          <h2>AddComment-component</h2>
-                <h3>box - Som användare vill jag kunna lägga en kommentar på alla mina tidsrapporter så jag minns vad jag jobbat med</h3>
                 <AddComment />
           ------------------------------------
         </div>
-
         <div>              
           ------------------------------------
           <h2>TimeLog-component</h2>
