4f2ed3a081cc36fac92d0f32e97ae39d02164550
Engwalls <erik.97@hotmail.com>
2023-03-22 13:43:26 +0100

    showing all the projects now in the ProjectList, and which status they have. Added some css to it also, just like the other ones that has to do with the projects.

--- src/components/ProjectsList.js
+++ src/components/ProjectsList.js
@@ -1,9 +1,76 @@
 import React from 'react'
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "../Css/loading.css";
+import "../Css/projectList.css";
 
 export function ProjectsList () {
-  return (
-    <div>
-        <h1>Visar alla project i databasen, aktuell samt färdiga projekt skapat av alla användare</h1>
-    </div>
-  )
+  const [error, setError] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const [SwaggerData, setSwaggerData] = useState([]);
+
+  useEffect(() => {
+    fetch('https://axb22z45ygh20230227215753.azurewebsites.net/get-all-projects')
+
+    .then((response) => response.json())
+    .then(
+      (result) => {
+        setLoaded(true);
+        setSwaggerData(result);
+        console.log(result);
+      },
+      (error) => {
+        setLoaded(true);
+        setError(error);
+      }
+    );
+  }, []);
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!loaded) {
+    return (
+      <div>
+          <h2 className="loadingTitle">Loading</h2>
+          <div className="spinner-border text-danger loadingIcon" role="status">
+          <span class="visually-hidden">Loading...</span>
+        </div>
+      </div>
+    );
+  } else {
+
+    const listofProjects = SwaggerData.filter(project => project.status === 'Active' || project.status === 'Inactive');
+
+
+    return (
+      <div className="bodyProjectList">
+        <div className="title-container">
+          <h1>Alla aktiva projekt</h1>
+        </div>
+        <div className="table-responsive">
+          <table className="table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Projekt namn</th>
+                <th>Beskrivning</th>
+                <th>Status</th>
+              </tr>
+            </thead>
+            <tbody className="table-group-divider">
+              {listofProjects.map((project) => (
+                <tr key={project.projectId}>
+                  <td>{project.projectId}</td>
+                  <td>{project.projectName}</td>
+                  <td>{project.description}</td>
+                  <td>{project.status}</td>
+                  <td colSpan="8" className="black-rows"></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    )
+  }
 }
