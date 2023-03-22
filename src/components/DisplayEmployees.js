33d6a60f2ea781e831db459f48048453f8f7053c
Engwalls <erik.97@hotmail.com>
2023-03-22 16:06:00 +0100

    Fixed a button to show the password and also hide it afterwards.

--- src/components/DisplayEmployees.js
+++ src/components/DisplayEmployees.js
@@ -8,6 +8,8 @@ export function DisplayEmployees() {
   const [error, setError] = useState(null);
   const [loaded, setLoaded] = useState(false);
   const [swaggerData, setSwaggerData] = useState([]);
+  const [passwordVisible, setPasswordVisible] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState("");
 
   useEffect(() => {
     fetch(
@@ -18,7 +20,6 @@ export function DisplayEmployees() {
         (result) => {
           setLoaded(true);
           setSwaggerData(result);
-          console.log(result);
         },
         (error) => {
           setLoaded(true);
@@ -27,15 +28,20 @@ export function DisplayEmployees() {
       );
   }, []);
 
+  const togglePasswordVisibility = (password) => {
+    setPasswordVisible(!passwordVisible);
+    setCurrentPassword(password);
+  };
+
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!loaded) {
     // loading data icon
     return (
       <>
-        <h2 class="loadingTitle">Loading</h2>
+        <h2 className="loadingTitle">Loading</h2>
         <div className="spinner-border text-danger loadingIcon" role="status">
-          <span class="visually-hidden">Loading...</span>
+          <span className="visually-hidden">Loading...</span>
         </div>
       </>
     );
@@ -46,35 +52,67 @@ export function DisplayEmployees() {
           <h1 className="displayTitle">Anställda i systemet</h1>
         </div>
         <div className="table-responsive">
-        <table className="table-striped">
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Roll</th>
-      <th>Förnamn</th>
-      <th>Efternamn</th>
-      <th>Adress</th>
-      <th>Postnummer</th>
-      <th>Ort</th>
-      <th>Lösenord</th>
-    </tr>
-  </thead>
-  <tbody className="table-group-divider">
-    {swaggerData.map((swaggerData) => (
-      <tr key={swaggerData.employeeId}>
-      <td>{swaggerData.employeeId}</td>
-      <td>{swaggerData.role}</td>
-      <td>{swaggerData.firstMidName}</td>
-      <td>{swaggerData.lastName}</td>
-      <td>{swaggerData.address}</td>
-      <td>{swaggerData.postalCode}</td>
-      <td>{swaggerData.city}</td>
-      <td>{swaggerData.password}</td>
-      <td colSpan="8" className="black-row"></td>
-    </tr>
-    ))}
-  </tbody>
-</table>
+          <table className="table-striped">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Roll</th>
+                <th>Förnamn</th>
+                <th>Efternamn</th>
+                <th>Adress</th>
+                <th>Postnummer</th>
+                <th>Ort</th>
+                <th>Lösenord</th>
+              </tr>
+            </thead>
+            <tbody className="table-group-divider">
+              {swaggerData.map((employee) => (
+                <tr key={employee.employeeId}>
+                  <td>{employee.employeeId}</td>
+                  <td>{employee.role}</td>
+                  <td>{employee.firstMidName}</td>
+                  <td>{employee.lastName}</td>
+                  <td>{employee.address}</td>
+                  <td>{employee.postalCode}</td>
+                  <td>{employee.city}</td>
+                  <td>
+                    {passwordVisible &&
+                    employee.password === currentPassword ? (
+                      <div>
+                        <span>{employee.password}</span>
+                        <button
+                          className="displayEmployee-btn"
+                          data-toggle="tooltip"
+                          data-placement="bottom"
+                          title="Klicka för att dölja lösenordet"
+                          onClick={() => setPasswordVisible(false)}
+                        >
+                          Dölj
+                        </button>
+                      </div>
+                    ) : (
+                      <div>
+                        <span>{"*".repeat(employee.password.length)}</span>
+                        <button
+                          className="displayEmployee-btn"
+                          data-toggle="tooltip"
+                          data-placement="bottom"
+                          title="Klicka för att visa lösenordet"
+                          onClick={() =>
+                            togglePasswordVisibility(employee.password)
+                          }
+                        >
+                          Visa
+                        </button>
+                      </div>
+                    )}
+                  </td>
+
+                  <td colSpan="8" className="black-row"></td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     );
